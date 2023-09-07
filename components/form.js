f00e220ed69d8cf899194b6bced1d60d41db18e2
Dima <kilunidmileoni@gmail.com>
2023-09-07 17:14:27 -0300

    Second commit

--- components/form.js
+++ components/form.js
@@ -13,20 +13,24 @@ export default function Form({ addArticle }) {
             {(props) => (
                 <View>
                     <TextInput
+                        style={styles.input}
                         value={props.values.name}
                         placeholder='Введите название'
                         onChangeText={props.handleChange('name')} />
                     <TextInput
+                        style={styles.input}
                         value={props.values.anons}
                         multiline
                         placeholder='Введите анонс'
                         onChangeText={props.handleChange('anons')} />
                     <TextInput
+                        style={styles.input}
                         value={props.values.full}
                         multiline
                         placeholder='Введите статью'
                         onChangeText={props.handleChange('full')} />
                     <TextInput
+                        style={styles.input}
                         value={props.values.img}
                         placeholder='Укажите фото'
                         onChangeText={props.handleChange('img')} />
@@ -39,5 +43,12 @@ export default function Form({ addArticle }) {
 }
 
 const styles = StyleSheet.create({
+    input: {
+        borderWidth:1,
+        marginTop: 15,
+        padding: 10,
+        borderColor: 'silver',
+        borderRadius: 5
+    }
 
 });
